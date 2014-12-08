e142f308155618b4f056ea9530846e6cb8b72150
Zubair Desai <zubaird@Zubairs-MacBook-Pro.local>
2014-12-08 09:59:23 -0800

    Create Products Table

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20141208175746) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: true do |t|
+    t.string  "name"
+    t.text    "description"
+    t.integer "price"
+  end
 
 end
