2516468fed399e3a22c157323295d9f96fb88c54
Zubair Desai <zubaird@Zubairs-MacBook-Pro.local>
2014-12-08 10:39:50 -0800

    Add timestamp fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141208175746) do
+ActiveRecord::Schema.define(version: 20141208183732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "products", force: true do |t|
-    t.string  "name"
-    t.text    "description"
-    t.integer "price"
+    t.string   "name"
+    t.text     "description"
+    t.integer  "price"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
