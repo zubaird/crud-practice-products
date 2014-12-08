be096f4fa93353aa7ef0355d508ac10287316add
Zubair Desai <zubaird@Zubairs-MacBook-Pro.local>
2014-12-08 10:29:36 -0800

    Add Edit Button and Edit Form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,30 +1,44 @@
 class ProductsController < ApplicationController
 
 
-def index
-  @products = Product.all
-end
-
-def new
-  @product = Product.new
-end
-
-def create
-  if @product = Product.create(product_params)
-    flash[:notice] = "Product successfully created"
-    redirect_to product_path(@product)
+  def index
+    @products = Product.all
   end
-end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    if @product = Product.create(product_params)
+      flash[:notice] = "Product successfully created"
+      redirect_to product_path(@product)
+    end
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+
+  end
 
 
-private
 
-def product_params
-  params.require(:product).permit(
-  :name,
-  :description,
-  :price,
-  )
-end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+    :name,
+    :description,
+    :price,
+    )
+  end
 
 end
