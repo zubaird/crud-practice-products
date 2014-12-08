03171190e009e4496026a8aa6124da34e030375b
Zubair Desai <zubaird@Zubairs-MacBook-Pro.local>
2014-12-08 10:14:47 -0800

    Users can create a product, and after creating it they are redirected to the show page

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,29 @@
+class ProductsController < ApplicationController
+
+
+def index
+  @products = Product.all
+end
+
+def new
+  @product = Product.new
+end
+
+def create
+  if @product = Product.create(product_params)
+    redirect_to product_path(@product)
+  end
+end
+
+
+private
+
+def product_params
+  params.require(:product).permit(
+  :name,
+  :description,
+  :price,
+  )
+end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Product.new({
+  name: "Test Product",
+  description: "Test decription for this product",
+  price: "34",
+  })
